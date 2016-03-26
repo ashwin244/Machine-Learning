6f8643c3e773315b0cff756e625e7b9e6bf6690f
Ashwin Raman <ashwin244@users.noreply.github.com>
2016-03-25 17:15:09 -0700

    Update ex2_reg.m

--- Week3/ex2_reg.m
+++ Week3/ex2_reg.m
@@ -61,7 +61,7 @@ X = mapFeature(X(:,1), X(:,2));
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1
-lambda = 0;
+lambda = 1;
 
 % Compute and display initial cost and gradient for regularized logistic
 % regression
@@ -87,7 +87,7 @@ pause;
 initial_theta = zeros(size(X, 2), 1);
 
 % Set regularization parameter lambda to 1 (you should vary this)
-lambda = 0;
+lambda = 1;
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
